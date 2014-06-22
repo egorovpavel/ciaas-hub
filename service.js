49356416e22057202f2d9fe8fd485cd24fe0d3e5
Pavel Egorov <pavel@egorov.ru>
2014-06-22 15:09:56 +0100

    simple mysql persistance implemented

--- service.js
+++ service.js
@@ -3,5 +3,5 @@ var Hub = require('./lib/hub.js');
 var Logger = require('winston');
 var persistanceHandler = require('./lib/SQLPersistanceHandler.js');
 var config = require('./config.json')[process.env.NODE_ENV || 'development'];
-var hub = new Hub(config, persistanceHandler,null,Logger);
+var hub = new Hub(config.redis, new persistanceHandler(config.mysql),null,Logger);
 hub.handle();
